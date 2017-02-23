094d68fae4cabd59b09bd6938935ad31e58ae1f4
Oren Me <oren.me@gmail.com>
2017-02-23 18:25:27 +0200

    AddMoreProjectSetup
    * Use flow ESlint plugin and global declaration location
    
    * Add ES6 instrumenting for Istanbul
    
    * Run Mocha recursively
    
    * Fix package.json dependency

--- test/util/stringUtil.spec.js
+++ test/util/stringUtil.spec.js
@@ -9,4 +9,9 @@ describe( 'capitlize', () => {
     capitlize( "first" ).charAt( 0 ).should.equal( 'F' );
   } );
 
+  it( 'should return the argument the same if it\'s not a string', () => {
+    let testArg = {"notString": true};
+    capitlize( testArg ).should.equal( testArg );
+  } );
+
 } );
