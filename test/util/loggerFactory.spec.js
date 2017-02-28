3c82ba7f4cdbf9a9499351e968128bbc317d9897
Dan Ziv <dan.ziv@kaltura.com>
2017-02-28 16:32:19 +0200

    feat(Logger): add a logger
    * Logger setup with examples and tests
    
    * Remove sinon from project dependencies
    
    * Delete unnecessary if in constructor
    
    * Updating module name
    
    * Fix import
    
    * Fix typing
    
    * Add player version printout
    
    * Indentation
    
    * Yarn

---
+++ test/util/loggerFactory.spec.js
@@ -0,0 +1,26 @@
+import chai from 'chai';
+import loggerFactory from '../../src/util/loggerFactory';
+import {LOG_LEVEL} from '../../src/util/loggerFactory';
+
+chai.should();
+
+let loggerA = null;
+let loggerB = null;
+
+describe( 'loggerFactory', () => {
+
+  it( 'should create two separate loggers', () => {
+    loggerA = loggerFactory.getLogger( 'loggerA' );
+    (loggerA.context.name).should.equal( 'loggerA' );
+
+    loggerB = loggerFactory.getLogger( 'loggerB' );
+    (loggerB.context.name).should.equal( 'loggerB' );
+  } );
+
+  it( 'should change loggerA\'s log level', () => {
+    loggerA.setLevel( LOG_LEVEL.WARN );
+    (loggerA.context.filterLevel).should.deep.equal( LOG_LEVEL.WARN );
+    (loggerB.context.filterLevel).should.deep.equal( LOG_LEVEL.DEBUG );
+  } );
+
+} );
