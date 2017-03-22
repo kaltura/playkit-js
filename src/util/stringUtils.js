ea038ff1e8fb3eae56b0c75756ab96759d6be302
Tanya Libatter <Tanya.Libatter@kaltura.com>
2017-03-22 16:22:05 +0200

    * add HttpRequestExecutor * add endsWith function to stringUtils

--- src/util/stringUtils.js
+++ src/util/stringUtils.js
@@ -16,4 +16,9 @@ function capitlize(string: string): string {
   }
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-export {capitlize};
+
+function endsWith(string: string, searchString: string): boolean {
+  return string.indexOf(searchString, string.length - searchString.length) != -1;
+}
+
+export {capitlize, endsWith};
