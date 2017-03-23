55994389a7fa59d78b0c1fd9db58262c9358ce7f
Tanya Libatter <tanya.libatter@kaltura.com>
2017-03-23 13:55:50 +0200

    feat(utils): add http and string utils

--- src/util/stringUtils.js
+++ src/util/stringUtils.js
@@ -16,4 +16,16 @@ function capitlize(string: string): string {
   }
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-export {capitlize};
+
+function endsWith(string: string, searchString: string): boolean {
+  if (typeof string !== 'string') {
+    return false;
+  }
+  if (typeof searchString !== 'string') {
+    return false;
+  }
+
+  return string.indexOf(searchString, string.length - searchString.length) != -1;
+}
+
+export {capitlize, endsWith};
