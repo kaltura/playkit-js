f650890c4469b07ee8d03b308e4430374c815c49
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2017-05-28 14:36:45 +0300

    export track class

--- src/track/index.js
+++ src/track/index.js
@@ -1,6 +1,7 @@
+import Track from './track'
 import VideoTrack from './video-track'
 import AudioTrack from './audio-track'
 import TextTrack from './text-track'
 import TRACK_TYPES from './track-types'
 
-export {VideoTrack, AudioTrack, TextTrack, TRACK_TYPES}
+export {Track, VideoTrack, AudioTrack, TextTrack, TRACK_TYPES}
