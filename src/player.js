031cca40f2a8688a1194051d4e898d9fee0f1b2c
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2017-06-27 12:51:21 +0300

    move no-source log

--- src/player.js
+++ src/player.js
@@ -109,14 +109,12 @@ export default class Player extends FakeEventTarget {
    * @returns {void}
    */
   configure(config: Object): void {
-    this._config = mergeDeep(Player._defaultConfig(), config);
-    if (this._selectEngine()) {
-      this._attachMedia();
-      this._loadPlugins();
-      this._handlePlaybackConfig();
-    } else {
-      Player._logger.warn("No playable engines was found to play the given sources");
-    }
+      this._config = mergeDeep(this._config || Player._defaultConfig(), config);
+      if (this._selectEngine()) {
+        this._attachMedia();
+        this._loadPlugins();
+        this._handlePlaybackConfig();
+      }
   }
 
   /**
@@ -194,6 +192,7 @@ export default class Player extends FakeEventTarget {
         }
       }
     }
+    Player._logger.warn("No playable engines was found to play the given sources");
     return false;
   }
 
