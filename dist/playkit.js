07bdc0cab8a258fcda6fff706e5693b6165483c8
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2017-07-23 17:13:08 +0300

    chore: update dist

--- dist/playkit.js
+++ dist/playkit.js
@@ -679,6 +679,18 @@ var _Generator = {
     var from = 2;
     var to = from + (!length || length < 0 ? 0 : length - 2);
     return '_' + Math.random().toString(36).substr(from, to);
+  },
+
+  /**
+   * Generates GUID.
+   * @return {string} - GUID
+   * @private
+   */
+  guid: function guid() {
+    var S4 = function S4() {
+      return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+    };
+    return S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4();
   }
 };
 
