1aab894045c71d68af18df3c45f691b09cc7d4f5
yair.ansbacher <Yair.Ansbacher@kaltura.com>
2017-07-23 17:12:42 +0300

    guid

--- src/utils/util.js
+++ src/utils/util.js
@@ -220,6 +220,18 @@ const _Generator = {
     let from = 2;
     let to = from + ((!length || length < 0) ? 0 : length - 2);
     return '_' + Math.random().toString(36).substr(from, to);
+  },
+
+  /**
+   * Generates GUID.
+   * @return {string} - GUID
+   * @private
+   */
+  guid: function (): string {
+    let S4 = () => {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
   }
 };
 
