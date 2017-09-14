c7a2995798cc859ab12db0f7aafea2a9793b32da
Yair Ansbacher <yair.ansbacher@kaltura.com>
2017-09-14 10:48:54 +0300

    feat(event-manager): support listen once and unlisten a specific listener (#111)

--- src/engines/html5/media-source/adapters/native-adapter.js
+++ src/engines/html5/media-source/adapters/native-adapter.js
@@ -181,14 +181,12 @@ export default class NativeAdapter extends BaseMediaSourceAdapter {
     if (!this._loadPromise) {
       this._loadPromise = new Promise((resolve, reject) => {
         // We're using 'loadeddata' event for native hls (on 'loadedmetadata' native hls doesn't have tracks yet).
-        this._eventManager.listen(this._videoElement, Html5Events.LOADED_DATA, () => {
-          this._eventManager.unlisten(this._videoElement, Html5Events.LOADED_DATA);
+        this._eventManager.listenOnce(this._videoElement, Html5Events.LOADED_DATA, () => {
           let data = {tracks: this._getParsedTracks()};
           NativeAdapter._logger.debug('The source has been loaded successfully');
           resolve(data);
         });
-        this._eventManager.listen(this._videoElement, Html5Events.ERROR, (error) => {
-          this._eventManager.unlisten(this._videoElement, Html5Events.ERROR);
+        this._eventManager.listenOnce(this._videoElement, Html5Events.ERROR, (error) => {
           NativeAdapter._logger.error(error);
           reject(error);
         });
@@ -379,10 +377,8 @@ export default class NativeAdapter extends BaseMediaSourceAdapter {
       let currentTime = this._videoElement.currentTime;
       let paused = this._videoElement.paused;
       this._sourceObj = videoTracks[videoTrack.index];
-      this._eventManager.listen(this._videoElement, Html5Events.LOADED_DATA, () => {
-        this._eventManager.unlisten(this._videoElement, Html5Events.LOADED_DATA);
-        this._eventManager.listen(this._videoElement, Html5Events.SEEKED, () => {
-          this._eventManager.unlisten(this._videoElement, Html5Events.SEEKED);
+      this._eventManager.listenOnce(this._videoElement, Html5Events.LOADED_DATA, () => {
+        this._eventManager.listenOnce(this._videoElement, Html5Events.SEEKED, () => {
           this._onTrackChanged(videoTrack);
         });
         this._videoElement.currentTime = currentTime;
---
+++ test/src/event/event-manager.spec.js
@@ -0,0 +1,86 @@
+import FakeEventTarget from '../../../src/event/fake-event-target'
+import EventManager from '../../../src/event/event-manager'
+
+class EventTarget extends FakeEventTarget {
+}
+
+describe("unlisten", function () {
+
+  let eventTarget, eventManager;
+
+  beforeEach(() => {
+    eventTarget = new EventTarget();
+    eventManager = new EventManager();
+  });
+
+  afterEach(() => {
+    eventManager.removeAll();
+  });
+
+  it("should remove all listeners for no specific listener given", (done) => {
+    let listener1 = () => {
+      done(new Error("test fail"));
+    };
+    let listener2 = () => {
+      done(new Error("test fail"));
+    };
+    eventManager.listen(eventTarget, 'event', listener1);
+    eventManager.listen(eventTarget, 'event', listener2);
+    eventManager.unlisten(eventTarget, 'event');
+    eventManager._bindingMap.get('event').length.should.equal(0);
+    setTimeout(() => {
+      done();
+    }, 0);
+  });
+
+  it("should remove only the given listeners", (done) => {
+    let listener1 = () => {
+      done(new Error("test fail"));
+    };
+    let listener2 = () => {
+      done();
+    };
+    eventManager.listen(eventTarget, 'event', listener1);
+    eventManager.listen(eventTarget, 'event', listener2);
+    eventManager.unlisten(eventTarget, 'event', listener1);
+    eventManager._bindingMap.get('event').length.should.equal(1);
+    eventManager._bindingMap.get('event')[0].listener.should.equal(listener2);
+    eventTarget.dispatchEvent({type: 'event'});
+  });
+});
+
+describe("one", function () {
+
+  let eventTarget, eventManager, listener1, listener2;
+
+  beforeEach(() => {
+    eventTarget = new EventTarget();
+    eventManager = new EventManager();
+  });
+
+  afterEach(() => {
+    eventManager.removeAll();
+  });
+
+  it("should listen only one time", (done) => {
+    let counter = 0;
+    listener1 = (event) => {
+      counter++;
+      event.type.should.equal('event1');
+    };
+    listener2 = (event) => {
+      counter++;
+      event.type.should.equal('event2');
+    };
+    eventManager.listenOnce(eventTarget, 'event1', listener1);
+    eventTarget.dispatchEvent({type: 'event1'});
+    eventTarget.dispatchEvent({type: 'event1'});
+    eventManager.listenOnce(eventTarget, 'event2', listener2);
+    eventTarget.dispatchEvent({type: 'event2'});
+    eventTarget.dispatchEvent({type: 'event2'});
+    setTimeout(() => {
+      counter.should.equal(2);
+      done();
+    }, 0);
+  });
+});
