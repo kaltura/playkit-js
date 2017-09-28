c6b744791d75df8557d15e33293258007d0e8ff8
Dan Ziv <dan.ziv@kaltura.com>
2017-09-28 14:33:47 +0300

    Remove comma

--- src/event/events.js
+++ src/event/events.js
@@ -89,7 +89,7 @@ const HTML5_EVENTS: { [event: string]: string } = {
   /**
    * Fires when the video stops because it needs to buffer the next frame
    */
-  WAITING: 'waiting',
+  WAITING: 'waiting'
 };
 
 const CUSTOM_EVENTS: { [event: string]: string } = {
