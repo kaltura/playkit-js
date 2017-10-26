98e7919cb5b458364a5cd31abc03981eefc2e82c
Dan Ziv <dan.ziv@kaltura.com>
2017-10-26 11:39:27 +0300

    fix(FEC-7356): player is loading twice for 'preload=auto' and 'autoplay=true' (#149)

--- src/player.js
+++ src/player.js
@@ -68,14 +68,12 @@ const SUBTITLES_CLASS_NAME: string = 'playkit-subtitles';
  */
 const LIVE: string = 'Live';
 
-
 /**
  *  The auto string, for captions
  *  @type {string}
  *  @const
  */
 const AUTO: string = 'auto';
-
 
 /**
  *  The off string, for captions
@@ -84,14 +82,12 @@ const AUTO: string = 'auto';
  */
 const OFF: string = 'off';
 
-
 /**
  *  The duration offset, for seeking to duration safety.
  *  @type {number}
  *  @const
  */
 const DURATION_OFFSET: number = 0.1;
-
 
 /**
  * The HTML5 player class.
@@ -286,7 +282,6 @@ export default class Player extends FakeEventTarget {
     textLanguage: ""
   };
 
-
   /**
    * @param {Object} config - The configuration for the player instance.
    * @constructor
@@ -335,7 +330,6 @@ export default class Player extends FakeEventTarget {
         this._posterManager.show();
         this._attachMedia();
         this._handlePlaybackOptions();
-        this._handlePreload();
         this._handleAutoPlay();
         if (receivedSourcesWhenHasEngine) {
           Player._logger.debug('Change source ended');
@@ -1140,21 +1134,21 @@ export default class Player extends FakeEventTarget {
     if (typeof this._config.playback.playsinline === 'boolean') {
       this.playsinline = this._config.playback.playsinline;
     }
+    if (this._canPreload()) {
+      this.load();
+    }
   }
 
   /**
-   * Handles preload.
    * If ads plugin enabled it's his responsibility to preload the content player.
    * So to avoid loading the player twice which can cause errors on MSEs we are not
    * calling load from the player.
    * TODO: Change it to check the ads configuration when we will develop the ads manager.
-   * @returns {void}
+   * @returns {boolean} - Whether the player should perform preload.
    * @private
    */
-  _handlePreload(): void {
-    if (this._config.playback.preload === "auto" && !this._config.plugins.ima) {
-      this.load();
-    }
+  _canPreload(): boolean {
+    return (!this._config.playback.autoplay && this._config.playback.preload === "auto" && !this._config.plugins.ima);
   }
 
   /**
