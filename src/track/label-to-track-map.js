8aa31407637ecf04531d2bcef9d9fa6314ca1360
odedhutzler <oded.hutzler@kaltura.com>
2018-04-09 11:26:44 +0300

    es6

--- src/track/label-to-track-map.js
+++ src/track/label-to-track-map.js
@@ -2,9 +2,10 @@
 import {TrackType} from './track-type'
 import {LabelOptions} from './label-options'
 
-const LabelToTrackMap = {};
-LabelToTrackMap[LabelOptions.AUDIO] = TrackType.AUDIO;
-LabelToTrackMap[LabelOptions.CAPTIONS] = TrackType.TEXT;
-LabelToTrackMap[LabelOptions.QUALITIES] = TrackType.VIDEO;
+const LabelToTrackMap = {
+  [LabelOptions.AUDIO]: TrackType.AUDIO,
+  [LabelOptions.CAPTIONS]: TrackType.TEXT,
+  [LabelOptions.QUALITIES]: TrackType.VIDEO
+};
 
 export {LabelToTrackMap};
