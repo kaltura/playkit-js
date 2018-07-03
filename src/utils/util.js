2771d4a4256501ba5a274b62659370a90b54ae77
Yair Ansbacher <yair.ansbacher@kaltura.com>
2018-07-03 13:26:59 +0300

    feat: version compare util (#257)
    from https://github.com/Rombecchi/version-compare

--- src/utils/util.js
+++ src/utils/util.js
@@ -490,11 +490,89 @@ const _Http = {
   jsonp: jsonp
 };
 
+const _VERSION = {
+  /**
+   * Compares two software version numbers (e.g. "1.7.1" or "1.2b").
+   *
+   * @param {string} v1 The first version to be compared.
+   * @param {string} v2 The second version to be compared.
+   * @param {object} [options] Optional flags that affect comparison behavior:
+   * lexicographical: (true/[false]) compares each part of the version strings lexicographically instead of naturally;
+   *                  this allows suffixes such as "b" or "dev" but will cause "1.10" to be considered smaller than "1.2".
+   * zeroExtend: ([true]/false) changes the result if one version string has less parts than the other. In
+   *             this case the shorter string will be padded with "zero" parts instead of being considered smaller.
+   *
+   * @returns {number|NaN}
+   * - 0 if the versions are equal
+   * - a negative integer iff v1 < v2
+   * - a positive integer iff v1 > v2
+   * - NaN if either version string is in the wrong format
+   */
+
+  compare: function (v1: string, v2: string, options: Object = {}) {
+    options = _Object.merge([{lexicographical: false, zeroExtend: true}, options]);
+    const lexicographical = options.lexicographical;
+    const zeroExtend = options.zeroExtend;
+    let v1parts = (v1 || "0").split('.');
+    let v2parts = (v2 || "0").split('.');
+
+    const isValidPart = (x) => {
+      return (lexicographical ? /^\d+[A-Za-zαß]*$/ : /^\d+[A-Za-zαß]?$/).test(x);
+    };
+
+    const mapParts = (parts: Array<string>) => {
+      return parts.map((x) => {
+        const match = (/[A-Za-zαß]/).exec(x);
+        return Number(match ? x.replace(match[0], "." + x.charCodeAt(match.index)) : x);
+      });
+    };
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+      return NaN;
+    }
+
+    if (zeroExtend) {
+      while (v1parts.length < v2parts.length) v1parts.push("0");
+      while (v2parts.length < v1parts.length) v2parts.push("0");
+    }
+
+    if (!lexicographical) {
+      v1parts = mapParts(v1parts);
+      v2parts = mapParts(v2parts);
+    }
+
+    for (let i = 0; i < v1parts.length; ++i) {
+      if (v2parts.length === i) {
+        return 1;
+      }
+
+      if (v1parts[i] === v2parts[i]) {
+        continue;
+      }
+      // $FlowFixMe
+      else if (v1parts[i] > v2parts[i]) {
+        return 1;
+      }
+      else {
+        return -1;
+      }
+    }
+
+    if (v1parts.length !== v2parts.length) {
+      return -1;
+    }
+
+    return 0;
+  }
+
+};
+
 export {
   _Number as Number,
   _String as String,
   _Object as Object,
   _Generator as Generator,
   _Dom as Dom,
-  _Http as Http
+  _Http as Http,
+  _VERSION as VERSION
 };
