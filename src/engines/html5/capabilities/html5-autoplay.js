c47d3b8d05c2a8e03c9c4baebb742e23e3959cc9
odedhutzler <oded.hutzler@kaltura.com>
2018-08-15 12:29:44 +0300

    changing the autoplay test
    - using a blob to test autoplay.
    - when the element.play() does not return a promise:
    1. if it's paused and return that autoplay is not available. (old iOS)
    2. if it's not - then return true (IE 11)

--- src/engines/html5/capabilities/html5-autoplay.js
+++ src/engines/html5/capabilities/html5-autoplay.js
@@ -1,16 +1,30 @@
 // @flow
 import * as Utils from '../../../utils/util';
-import * as EncodingSources from '../../../assets/encoding-sources.json';
 import {Html5EventType} from '../../../event/event-type';
+import getLogger from '../../../utils/logger';
 
 const WAIT_TIME: number = 500;
 
+/**
+ * @type {Blob}
+ */
+// eslint-disable-next-line
+const VIDEO = new Blob([new Uint8Array([0, 0, 0, 28, 102, 116, 121, 112, 105, 115, 111, 109, 0, 0, 2, 0, 105, 115, 111, 109, 105, 115, 111, 50, 109, 112, 52, 49, 0, 0, 0, 8, 102, 114, 101, 101, 0, 0, 2, 239, 109, 100, 97, 116, 33, 16, 5, 32, 164, 27, 255, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 167, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 112, 33, 16, 5, 32, 164, 27, 255, 192, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 167, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 112, 0, 0, 2, 194, 109, 111, 111, 118, 0, 0, 0, 108, 109, 118, 104, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 232, 0, 0, 0, 47, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 236, 116, 114, 97, 107, 0, 0, 0, 92, 116, 107, 104, 100, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 47, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 101, 100, 116, 115, 0, 0, 0, 28, 101, 108, 115, 116, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 47, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 100, 109, 100, 105, 97, 0, 0, 0, 32, 109, 100, 104, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 172, 68, 0, 0, 8, 0, 85, 196, 0, 0, 0, 0, 0, 45, 104, 100, 108, 114, 0, 0, 0, 0, 0, 0, 0, 0, 115, 111, 117, 110, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 83, 111, 117, 110, 100, 72, 97, 110, 100, 108, 101, 114, 0, 0, 0, 1, 15, 109, 105, 110, 102, 0, 0, 0, 16, 115, 109, 104, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 36, 100, 105, 110, 102, 0, 0, 0, 28, 100, 114, 101, 102, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 12, 117, 114, 108, 32, 0, 0, 0, 1, 0, 0, 0, 211, 115, 116, 98, 108, 0, 0, 0, 103, 115, 116, 115, 100, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 87, 109, 112, 52, 97, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 16, 0, 0, 0, 0, 172, 68, 0, 0, 0, 0, 0, 51, 101, 115, 100, 115, 0, 0, 0, 0, 3, 128, 128, 128, 34, 0, 2, 0, 4, 128, 128, 128, 20, 64, 21, 0, 0, 0, 0, 1, 244, 0, 0, 1, 243, 249, 5, 128, 128, 128, 2, 18, 16, 6, 128, 128, 128, 1, 2, 0, 0, 0, 24, 115, 116, 116, 115, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 28, 115, 116, 115, 99, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 28, 115, 116, 115, 122, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 115, 0, 0, 1, 116, 0, 0, 0, 20, 115, 116, 99, 111, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 44, 0, 0, 0, 98, 117, 100, 116, 97, 0, 0, 0, 90, 109, 101, 116, 97, 0, 0, 0, 0, 0, 0, 0, 33, 104, 100, 108, 114, 0, 0, 0, 0, 0, 0, 0, 0, 109, 100, 105, 114, 97, 112, 112, 108, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 105, 108, 115, 116, 0, 0, 0, 37, 169, 116, 111, 111, 0, 0, 0, 29, 100, 97, 116, 97, 0, 0, 0, 1, 0, 0, 0, 0, 76, 97, 118, 102, 53, 54, 46, 52, 48, 46, 49, 48, 49])], {type: 'video/mp4'});
+
 const testVideoElement: HTMLVideoElement = Utils.Dom.createElement('video');
-testVideoElement.src = EncodingSources.Base64Mp4Source;
+testVideoElement.src = URL.createObjectURL(VIDEO);
 // For iOS devices needs to turn the playsinline attribute on
 testVideoElement.setAttribute('playsinline', '');
 
 export default class Html5AutoPlayCapability implements ICapability {
+  /**
+   * The html5 class logger.
+   * @type {any}
+   * @static
+   * @private
+   */
+  static _logger: any = getLogger('Html5AutoPlayCapability');
+
   static _playPromiseResult: Promise<*>;
 
   /**
@@ -21,15 +35,19 @@ export default class Html5AutoPlayCapability implements ICapability {
    */
   static runCapability(): void {
     Html5AutoPlayCapability._playPromiseResult = new Promise(resolve => {
-      Html5AutoPlayCapability._setMuted(false);
-      Html5AutoPlayCapability._getPlayPromise()
-        .then(() => resolve({autoplay: true, mutedAutoPlay: true}))
-        .catch(() => {
-          Html5AutoPlayCapability._setMuted(true);
-          Html5AutoPlayCapability._getPlayPromise()
-            .then(() => resolve({autoplay: false, mutedAutoPlay: true}))
-            .catch(() => resolve({autoplay: false, mutedAutoPlay: false}));
-        });
+      Html5AutoPlayCapability._getPlayPromise(false).then(result => {
+        if (result) {
+          return resolve({autoplay: true, mutedAutoPlay: true});
+        } else {
+          Html5AutoPlayCapability._getPlayPromise(true).then(mutedResult => {
+            if (mutedResult) {
+              return resolve({autoplay: false, mutedAutoPlay: true});
+            } else {
+              return resolve({autoplay: false, mutedAutoPlay: false});
+            }
+          });
+        }
+      });
     });
   }
 
@@ -52,11 +70,37 @@ export default class Html5AutoPlayCapability implements ICapability {
 
   /**
    * Gets the play promise.
+   * @param {boolean} muted - if the element should be muted or not.
    * @return {Promise<*>} - Play promise which resolved or rejected.
    * @private
    */
-  static _getPlayPromise(): Promise<*> {
-    return testVideoElement.play() || Html5AutoPlayCapability._forcePromiseReturnValue();
+  static _getPlayPromise(muted: boolean): Promise<*> {
+    return new Promise(resolve => {
+      const sendOutput = result => {
+        clearTimeout(timeoutId);
+        resolve(result);
+      };
+      Html5AutoPlayCapability._setMuted(muted);
+      testVideoElement.addEventListener(Html5EventType.error, error => {
+        Html5AutoPlayCapability._logger.debug(`Play promise caused an error when trying to play the blob`, error);
+        return resolve(false);
+      });
+      const playResult = testVideoElement.play();
+      const timeoutId = setTimeout(() => {
+        Html5AutoPlayCapability._logger.debug(`Timeout ${WAIT_TIME} ms has been reached`);
+        sendOutput(false);
+      }, WAIT_TIME);
+      if (playResult) {
+        playResult.then(() => sendOutput(true)).catch(() => sendOutput(false));
+      } else {
+        Html5AutoPlayCapability._logger.debug(`Play Promise returned undefined`);
+        if (testVideoElement.paused === true) {
+          sendOutput(false);
+        } else {
+          sendOutput(true);
+        }
+      }
+    });
   }
 
   /**
@@ -74,28 +118,5 @@ export default class Html5AutoPlayCapability implements ICapability {
       testVideoElement.muted = false;
       testVideoElement.removeAttribute('muted');
     }
-  }
-
-  /**
-   * For browsers which are not supported promise return value from play()
-   * request we are simulate the same behaviour.
-   * @return {Promise<*>} - Play promise which resolved or rejected.
-   * @private
-   * @static
-   */
-  static _forcePromiseReturnValue(): Promise<*> {
-    return new Promise((resolve, reject) => {
-      const supported = setTimeout(() => {
-        reject();
-      }, WAIT_TIME);
-      testVideoElement.addEventListener(Html5EventType.PLAYING, () => {
-        clearTimeout(supported);
-        resolve();
-      });
-      testVideoElement.addEventListener(Html5EventType.ERROR, () => {
-        clearTimeout(supported);
-        reject();
-      });
-    });
   }
 }
