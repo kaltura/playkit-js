5e2ea63619b7e63f924dd86b6b19057e61cebb27
yair.ansbacher <yairans@gmail.com>
2018-09-03 23:47:47 +0300

    jsdoc

--- src/controller/controller-provider.js
+++ src/controller/controller-provider.js
@@ -2,14 +2,26 @@
 
 import PluginManager from '../plugin/plugin-manager';
 
+/**
+ * Controller provider
+ * @classdesc
+ */
 class ControllerProvider {
   _pluginManager: PluginManager;
 
+  /**
+   * @constructor
+   * @param {PluginManager} pluginManager - the plugin manager
+   */
   constructor(pluginManager: PluginManager) {
     this._pluginManager = pluginManager;
   }
 
-  getAdsController(): IAdsController {
+  /**
+   * Get the ads controller of the current ads plugin.
+   * @returns {?IAdsController} - the ads controller.
+   */
+  getAdsController(): ?IAdsController {
     // $FlowFixMe
     const adPlugin: IAdsControllerProvider = this._pluginManager.getAll().find(plugin => typeof plugin.getAdsController === 'function');
     return adPlugin && adPlugin.getAdsController();
