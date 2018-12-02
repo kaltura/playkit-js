46b492849f8ba767fa2e37c54eab745ab29c61f6
Oded Hutzler <31587052+odedhutzler@users.noreply.github.com>
2018-12-02 09:36:23 +0200

    fix(FEC-8714): change media doesn't work after player.destroy() on iOS (#317)
    1. creating the player video element before running the capabilites so a click would affect it as well as the one we create in 'run capabilites'
    2. reseting the static Player._playerCapabilities. Because after a 'click' it is set to auto play. After change media - it's not always the case

--- src/player.js
+++ src/player.js
@@ -400,6 +400,7 @@ export default class Player extends FakeEventTarget {
    */
   constructor(config: Object = {}) {
     super();
+    Player._prepareVideoElement();
     Player.runCapabilities();
     this._env = Env;
     this._tracks = [];
@@ -608,6 +609,7 @@ export default class Player extends FakeEventTarget {
     if (this._destroyed) return;
     //make sure all services are destroyed before engine and engine attributes are destroyed
     this._externalCaptionsHandler.destroy();
+    Player._playerCapabilities = {};
     this._posterManager.destroy();
     this._pluginManager.destroy();
     this._stateManager.destroy();
