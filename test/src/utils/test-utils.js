f7a6b401e4a531ae5654ac47773eb62daca09fa9
Shalom Meoded <shalom.meoded@kaltura.com>
2019-05-29 18:32:17 +0300

    Change the test

--- test/src/utils/test-utils.js
+++ test/src/utils/test-utils.js
@@ -10,7 +10,7 @@ function getConfigStructure() {
       preload: 'none',
       autoplay: false,
       muted: false,
-      playbackRate: [2, 4, 3],
+      playbackRate: [2, 3, 4],
       streamPriority: [
         {
           engine: 'html5',
