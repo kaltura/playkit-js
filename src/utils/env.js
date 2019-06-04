f8d384153f2c77f97f44b63ab540ade04e01b113
Yuval keidar <yuval.keidar@kaltura.com>
2019-06-04 17:25:23 +0300

    fix(FEC-9177): Smart TV showed as mobile device
    Use UAParser enum for device type and add check for webos smart tv as extend of this lib.

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,15 @@
 // @flow
 import UAParser from 'ua-parser-js';
-const Env = new UAParser().getResult();
+
+const LGDeviceRegex = [[/^.*(web0s).*(smarttv).*$/i], [[UAParser.DEVICE.VENDOR, 'LG'], [UAParser.DEVICE.TYPE, UAParser.DEVICE.SMARTTV]]];
+const LGOSRegex = [[/^.*(web0s).*(smarttv).*$/i], [UAParser.OS.NAME]];
+
+let Env = new UAParser(undefined, {device: LGDeviceRegex, os: LGOSRegex}).getResult();
+
+Env.isSmartTV = Env.device.type === UAParser.DEVICE.SMARTTV;
+Env.isMobile = Env.device.type === UAParser.DEVICE.MOBILE;
+Env.isTablet = Env.device.type === UAParser.DEVICE.TABLET;
+Env.isWearable = Env.device.type === UAParser.DEVICE.WEARABLE;
+Env.isEmbedded = Env.device.type === UAParser.DEVICE.EMBEDDED;
+
 export default Env;
