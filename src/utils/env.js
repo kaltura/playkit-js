d4e5247ce02e065f2a92e31aa327cd63814c583c
Yuval keidar <yuval.keidar@kaltura.com>
2019-06-04 17:31:57 +0300

    Update convention

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,10 +1,11 @@
 // @flow
 import UAParser from 'ua-parser-js';
 
-const LGDeviceRegex = [[/^.*(web0s).*(smarttv).*$/i], [[UAParser.DEVICE.VENDOR, 'LG'], [UAParser.DEVICE.TYPE, UAParser.DEVICE.SMARTTV]]];
-const LGOSRegex = [[/^.*(web0s).*(smarttv).*$/i], [UAParser.OS.NAME]];
+const LGTVRegex = /^.*(web0s).*(smarttv).*$/i;
+const LGDeviceParser = [[LGTVRegex], [[UAParser.DEVICE.VENDOR, 'LG'], [UAParser.DEVICE.TYPE, UAParser.DEVICE.SMARTTV]]];
+const LGOSParser = [[LGTVRegex], [UAParser.OS.NAME]];
 
-let Env = new UAParser(undefined, {device: LGDeviceRegex, os: LGOSRegex}).getResult();
+let Env = new UAParser(undefined, {device: LGDeviceParser, os: LGOSParser}).getResult();
 
 Env.isSmartTV = Env.device.type === UAParser.DEVICE.SMARTTV;
 Env.isMobile = Env.device.type === UAParser.DEVICE.MOBILE;
