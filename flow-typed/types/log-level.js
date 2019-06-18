08163a82a974d15f677d71ac50aeb98f91670b86
Yuval keidar <yuval.keidar@kaltura.com>
2019-06-18 17:19:40 +0300

    feat(FEC-9023): More logger options
    add configuration logger options.
    handler to output logs with custimize function.
    option to remove playerVersion from log.

--- flow-typed/types/log-level.js
+++ flow-typed/types/log-level.js
@@ -2,3 +2,9 @@
 declare type PKLogLevelObject = { value: number, name: string };
 declare type PKLogLevels = { [level: string]: PKLogLevelObject };
 declare type PKLogLevelTypes = { [level: string]: string };
+declare type LogHandlerType = (messages: any[], context: Object) => void;
+declare type PKLogConfigObject = {
+  level:string,
+  playerVersion:boolean,
+  handler:LogHandlerType
+};
