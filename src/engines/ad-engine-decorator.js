74baa0e43edaed8dcd3c526a2de9f2e225909626
yair.ansbacher <yairans@gmail.com>
2019-06-27 13:41:20 +0300

    use proxy for ad decorator

--- src/engines/ad-engine-decorator.js
+++ src/engines/ad-engine-decorator.js
@@ -1,13 +1,15 @@
 // @flow
 import {BaseEngineDecorator} from './base-engine-decorator';
 import {EventType} from '../event/event-type';
+import FakeEventTarget from '../event/fake-event-target';
+import EventManager from '../event/event-manager';
 
 /**
  * Engine decorator for ad plugin.
  * @class AdEngineDecorator
  * @param {IEngine} engine - The engine to decorate.
  */
-class AdEngineDecorator extends BaseEngineDecorator {
+class AdEngineDecorator extends FakeEventTarget implements IEngine {
   _pluginDecorators;
 
   static getDecorator(engine: IEngine, plugins: Array<BasePlugin>): ?BaseEngineDecorator {
@@ -16,62 +18,30 @@ class AdEngineDecorator extends BaseEngineDecorator {
   }
 
   constructor(engine: IEngine, pluginDecorators) {
-    super(engine);
+    super();
+    this._eventManager = new EventManager();
     this._pluginDecorators = pluginDecorators;
     const events: Array<string> = (Object.values(EventType): any);
-    events.forEach(event => this._eventManager.listen(this._engine, event, e => this.dispatchEvent(e)));
+    events.forEach(event => this._eventManager.listen(engine, event, e => this.dispatchEvent(e)));
+    return new Proxy(engine, {
+      get: (obj, prop) => {
+        if (prop === '_listeners') {
+          return this._listeners;
+        }
+        const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
+        return activeDecorator ? activeDecorator[prop] : obj[prop];
+      },
+      set: (obj, prop, value) => {
+        const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
+        activeDecorator ? (activeDecorator[prop] = value) : (obj[prop] = value);
+        return true;
+      }
+    });
   }
 
   dispatchEvent(event: FakeEvent): boolean {
     const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
     return activeDecorator ? activeDecorator.dispatchEvent(event) : super.dispatchEvent(event);
-  }
-
-  /**
-   * Get paused state.
-   * @returns {boolean} - The paused value of the engine.
-   * @public
-   * @override
-   * @instance
-   * @memberof AdEngineDecorator
-   */
-  get paused(): boolean {
-    const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
-    return activeDecorator ? activeDecorator.paused : super.paused;
-  }
-  /**
-   * Get the duration in seconds.
-   * @returns {number} - The playback duration.
-   * @public
-   * @override
-   * @instance
-   * @memberof AdEngineDecorator
-   */
-  get duration(): ?number {
-    const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
-    return activeDecorator ? activeDecorator.duration : super.duration;
-  }
-  /**
-   * Get the current time in seconds.
-   * @returns {number} - The current playback time.
-   * @public
-   * @override
-   * @instance
-   * @memberof AdEngineDecorator
-   */
-  get currentTime(): number {
-    const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
-    return activeDecorator ? activeDecorator.currentTime : super.currentTime;
-  }
-  /**
-   * Set the current time in seconds.
-   * @param {number} to - The number to set in seconds.
-   * @public
-   * @returns {void}
-   */
-  set currentTime(to: number): void {
-    const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
-    activeDecorator ? (activeDecorator.currentTime = to) : (super.currentTime = to);
   }
 }
 
