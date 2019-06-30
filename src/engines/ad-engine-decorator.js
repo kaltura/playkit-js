1adab64efe594f09085c485b408f166a1c4006b9
yair.ansbacher <yairans@gmail.com>
2019-06-30 15:30:32 +0300

    remove BaseEngineDecorator

--- src/engines/ad-engine-decorator.js
+++ src/engines/ad-engine-decorator.js
@@ -1,5 +1,4 @@
 // @flow
-import {BaseEngineDecorator} from './base-engine-decorator';
 import {EventType} from '../event/event-type';
 import FakeEventTarget from '../event/fake-event-target';
 import EventManager from '../event/event-manager';
@@ -12,15 +11,15 @@ import EventManager from '../event/event-manager';
 class AdEngineDecorator extends FakeEventTarget implements IEngine {
   _pluginDecorators;
 
-  static getDecorator(engine: IEngine, plugins: Array<BasePlugin>): ?BaseEngineDecorator {
-    const pluginDecorators = plugins.filter(plugin => plugin.getEngineDecorator).map(plugin => plugin.getEngineDecorator(engine));
-    return pluginDecorators.length ? new this(engine, pluginDecorators) : null;
+  static getDecorator(engine: IEngine, plugins: Array<BasePlugin>): ?IEngine {
+    const pluginWithDecorators = plugins.filter(plugin => plugin.getEngineDecorator);
+    return pluginWithDecorators.length ? new this(engine, pluginWithDecorators) : null;
   }
 
-  constructor(engine: IEngine, pluginDecorators) {
+  constructor(engine: IEngine, pluginWithDecorators) {
     super();
     this._eventManager = new EventManager();
-    this._pluginDecorators = pluginDecorators;
+    this._pluginDecorators = pluginWithDecorators.map(plugin => plugin.getEngineDecorator(engine, super.dispatchEvent.bind(this)));
     const events: Array<string> = (Object.values(EventType): any);
     events.forEach(event => this._eventManager.listen(engine, event, e => this.dispatchEvent(e)));
     return new Proxy(engine, {
@@ -28,11 +27,11 @@ class AdEngineDecorator extends FakeEventTarget implements IEngine {
         if (prop === '_listeners') {
           return this._listeners;
         }
-        const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
+        const activeDecorator = this._pluginDecorators.find(decorator => prop in decorator && decorator.active);
         return activeDecorator ? activeDecorator[prop] : obj[prop];
       },
       set: (obj, prop, value) => {
-        const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
+        const activeDecorator = this._pluginDecorators.find(decorator => prop in decorator && decorator.active);
         activeDecorator ? (activeDecorator[prop] = value) : (obj[prop] = value);
         return true;
       }
@@ -41,7 +40,7 @@ class AdEngineDecorator extends FakeEventTarget implements IEngine {
 
   dispatchEvent(event: FakeEvent): boolean {
     const activeDecorator = this._pluginDecorators.find(decorator => decorator.active);
-    return activeDecorator ? activeDecorator.dispatchEvent(event) : super.dispatchEvent(event);
+    return activeDecorator ? activeDecorator.dispatchEvent && activeDecorator.dispatchEvent(event) : super.dispatchEvent(event);
   }
 }
 
