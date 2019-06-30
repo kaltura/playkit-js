f814c8201385fb44f876a62c7ee7df8e633d8550
yair.ansbacher <yairans@gmail.com>
2019-06-30 16:44:05 +0300

    flow

--- src/engines/ad-engine-decorator.js
+++ src/engines/ad-engine-decorator.js
@@ -1,22 +1,25 @@
 // @flow
+import FakeEvent from '../event/fake-event';
 import {EventType} from '../event/event-type';
-import FakeEventTarget from '../event/fake-event-target';
 import EventManager from '../event/event-manager';
+import FakeEventTarget from '../event/fake-event-target';
 
 /**
  * Engine decorator for ad plugin.
  * @class AdEngineDecorator
  * @param {IEngine} engine - The engine to decorate.
  */
+// $FlowFixMe
 class AdEngineDecorator extends FakeEventTarget implements IEngine {
-  _pluginDecorators;
+  _pluginDecorators: Array<any>;
+  _eventManager: EventManager;
 
-  static getDecorator(engine: IEngine, plugins: Array<BasePlugin>): ?IEngine {
+  static getDecorator(engine: IEngine, plugins: Array<any>): ?IEngine {
     const pluginWithDecorators = plugins.filter(plugin => plugin.getEngineDecorator);
     return pluginWithDecorators.length ? new this(engine, pluginWithDecorators) : null;
   }
 
-  constructor(engine: IEngine, pluginWithDecorators) {
+  constructor(engine: IEngine, pluginWithDecorators: Array<IEngineDecoratorProvider>) {
     super();
     this._eventManager = new EventManager();
     this._pluginDecorators = pluginWithDecorators.map(plugin => plugin.getEngineDecorator(engine, super.dispatchEvent.bind(this)));
@@ -28,10 +31,12 @@ class AdEngineDecorator extends FakeEventTarget implements IEngine {
           return this._listeners;
         }
         const activeDecorator = this._pluginDecorators.find(decorator => prop in decorator && decorator.active);
+        // $FlowFixMe
         return activeDecorator ? activeDecorator[prop] : obj[prop];
       },
       set: (obj, prop, value) => {
         const activeDecorator = this._pluginDecorators.find(decorator => prop in decorator && decorator.active);
+        // $FlowFixMe
         activeDecorator ? (activeDecorator[prop] = value) : (obj[prop] = value);
         return true;
       }
