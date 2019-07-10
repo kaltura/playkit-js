e68e85cd59c92cf020532e08d7c2e38302d3d130
Eran Sakal <eran@sakalim.com>
2019-07-10 14:49:47 +0300

    use render approach instead of create/destroy when injecting preset components

--- flow-typed/types/preset.types.js
+++ flow-typed/types/preset.types.js
@@ -3,8 +3,7 @@ type PresetComponent = {
   label: string,
   presets?: Array<string>,
   container: string,
-  create: (options: {context?: any, parent: HTMLElement}) => void,
-  onDestroy: (options: {context?: any, parent: HTMLElement}) => void,
+  render: (options: { context?: any}) => any
   beforeComponent?: string,
   afterComponent?: string,
   replaceComponent?: string,
