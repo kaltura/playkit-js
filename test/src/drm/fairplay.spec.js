301004c03c3e31fd4e582ac3353a9170f2220598
Yuval keidar <yuval.keidar@kaltura.com>
2019-08-21 14:09:50 +0300

    fix tests

--- test/src/drm/fairplay.spec.js
+++ test/src/drm/fairplay.spec.js
@@ -1,13 +1,7 @@
 import FairPlay from '../../../src/drm/fairplay';
-import Env from '../../../src/utils/env';
 
-const BROWSER: string = Env.browser.name;
 const fpDrmData = [{licenseUrl: 'LICENSE_URL', scheme: FairPlay.DrmScheme.FAIRPLAY}];
 const wwDrmData = [{licenseUrl: 'LICENSE_URL', scheme: FairPlay.DrmScheme.WIDEVINE}];
-
-function isValidEnvForFairPlay() {
-  return BROWSER === 'Safari' && FairPlay.DrmSupport._Browsers['Safari']() === FairPlay.DrmScheme.FAIRPLAY;
-}
 
 describe('FairPlay', function() {
   describe('isConfigured', function() {
@@ -21,20 +15,6 @@ describe('FairPlay', function() {
 
     it('should return false for non-fairplay data even configured', function() {
       FairPlay.isConfigured(wwDrmData, {keySystem: FairPlay.DrmScheme.FAIRPLAY}).should.be.false;
-    });
-  });
-
-  describe('canPlayDrm', function() {
-    it('should return true for fairplay data on valid fairplay env and false otherwise', function() {
-      if (isValidEnvForFairPlay()) {
-        FairPlay.canPlayDrm(fpDrmData).should.be.true;
-      } else {
-        FairPlay.canPlayDrm(fpDrmData).should.be.false;
-      }
-    });
-
-    it('should return false for non-fairplay data in any case', function() {
-      FairPlay.canPlayDrm(wwDrmData).should.be.false;
     });
   });
 });
