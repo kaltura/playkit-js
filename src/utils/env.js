8363d6a0458411a40c801f1cd002b3b87c6aef6f
Yuvalke <48407737+Yuvalke@users.noreply.github.com>
2019-08-28 15:06:53 +0300

    fix: browser detection for samsung (#394)
    browser detection for Samsung as safari and to be detected as SmartTV

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,10 +2,25 @@
 import UAParser from 'ua-parser-js';
 
 const LGTVRegex = /^.*(web0s).*(smarttv).*$/i;
-const LGDeviceParser = [[LGTVRegex], [[UAParser.DEVICE.VENDOR, 'LG'], [UAParser.DEVICE.TYPE, UAParser.DEVICE.SMARTTV]]];
+//doesn't recognize lg at all
 const LGOSParser = [[LGTVRegex], [UAParser.OS.NAME]];
 
-let Env = new UAParser(undefined, {device: LGDeviceParser, os: LGOSParser}).getResult();
+const SAMSUNGTVRegex = /^.*(smart-tv).*(tizen).*$/i;
+//recognize as safari
+const SAMSUNGBrowserParser = [
+  [SAMSUNGTVRegex],
+  [[UAParser.BROWSER.NAME, 'SAMSUNG_TV_BROWSER'], [UAParser.BROWSER.MAJOR, ''], [UAParser.BROWSER.VERSION, '']]
+];
+
+//add smart tv as smart tv devices
+const DeviceParser = [
+  [LGTVRegex],
+  [[UAParser.DEVICE.VENDOR, 'LG'], [UAParser.DEVICE.TYPE, UAParser.DEVICE.SMARTTV]],
+  [SAMSUNGTVRegex],
+  [[UAParser.DEVICE.TYPE, UAParser.DEVICE.SMARTTV]]
+];
+
+let Env = new UAParser(undefined, {browser: SAMSUNGBrowserParser, device: DeviceParser, os: LGOSParser}).getResult();
 
 Env.isConsole = Env.device.type === UAParser.DEVICE.CONSOLE;
 Env.isSmartTV = Env.device.type === UAParser.DEVICE.SMARTTV;
