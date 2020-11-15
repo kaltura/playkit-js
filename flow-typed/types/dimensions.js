7d8668d2efa33b14a875ebd9f5c33d0e900dadcc
Dan Ziv <dan.ziv@kaltura.com>
2020-11-15 13:25:16 +0200

    FEC-10632

---
+++ flow-typed/types/dimensions.js
@@ -0,0 +1,10 @@
+declare type PKPlayerDimensions = {
+  width?: number | string,
+  height?: number | string
+};
+
+declare type PKDimensionsConfig = {
+  width?: number | string,
+  height?: number | string,
+  ratio?: string
+};
--- src/player.js
+++ src/player.js
@@ -454,6 +454,7 @@ export default class Player extends FakeEventTarget {
         this._attachMedia();
         this._handlePlaybackOptions();
         this._posterManager.setSrc(this._config.sources.poster);
+        this._handleDimensions();
         this._handlePreload();
         this._handleAutoPlay();
         Player._logger.debug('Change source ended');
@@ -879,6 +880,46 @@ export default class Player extends FakeEventTarget {
       return this._engine.src;
     }
     return null;
+  }
+
+  /**
+   * Sets the dimensions of the player.
+   * @param {PKPlayerDimensions} dimensions - the player dimensions input.
+   * @returns {void}
+   * @public
+   */
+  set dimensions(dimensions?: PKPlayerDimensions) {
+    const targetElement = document.getElementById(this.config.targetId);
+    if (!dimensions || Utils.Object.isEmptyObject(dimensions)) {
+      targetElement.style.width = null;
+      targetElement.style.height = null;
+    } else {
+      const {height, width} = {...this.dimensions, ...dimensions};
+      targetElement.style.width = typeof width === 'number' ? `${width}px` : width;
+      targetElement.style.height = typeof height === 'number' ? `${height}px` : height;
+    }
+    if (Utils.Object.hasPropertyPath(this.config, 'dimensions.ratio')) {
+      this._calcRatio(targetElement, dimensions);
+    }
+  }
+
+  /**
+   * Calc the aspect ratio of the player.
+   * @param {HTMLDivElement} targetElement - the player root element.
+   * @param {PKPlayerDimensions} dimensions - the player dimensions input.
+   * @returns {void}
+   * @public
+   */
+  _calcRatio(targetElement: HTMLDivElement, dimensions: PKPlayerDimensions) {
+    const {ratio} = this.config.dimensions;
+    const [ratioWidth, ratioHeight] = ratio.split(':').map(r => Number(r));
+    if (dimensions.width || (!dimensions.width && !dimensions.height)) {
+      const height = (ratioHeight / ratioWidth) * targetElement.clientWidth;
+      targetElement.style.height = `${height}px`;
+    } else if (dimensions.height && !dimensions.width) {
+      const width = (ratioWidth / ratioHeight) * targetElement.clientHeight;
+      targetElement.style.width = `${width}px`;
+    }
   }
 
   /**
@@ -1911,6 +1952,13 @@ export default class Player extends FakeEventTarget {
           this.dispatchEvent(new FakeEvent(Html5EventType.ERROR, error));
           resetFlags();
         });
+    }
+  }
+
+  _handleDimensions(): void {
+    const {dimensions} = this.config;
+    if (Utils.Object.isObject(dimensions) && !Utils.Object.isEmptyObject(dimensions)) {
+      this.dimensions = dimensions;
     }
   }
 
