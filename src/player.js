968453ee89945b7c569c00b9d79022c387efb0a3
yuval.keidar <keidaryu@gmail.com>
2020-11-18 18:14:02 +0200

    fix flow

--- src/player.js
+++ src/player.js
@@ -2070,7 +2070,7 @@ export default class Player extends FakeEventTarget {
         const videoElement = this.getVideoElement();
         return videoElement
           ? Array.from(videoElement.textTracks).findIndex(track =>
-              track ? track.language === textTrack.language || (!!textTrack.external && track.language === EXTERNAL_TRACK_ID) : false
+              track ? track.language === textTrack.language || track.language === EXTERNAL_TRACK_ID : false
             )
           : -1;
       };
