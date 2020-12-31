2e108a33cc97899d657473c99ccedb76131cf150
yuval.keidar <keidaryu@gmail.com>
2020-12-31 18:27:27 +0200

    update playkit to work with number of commit from master

--- scripts/update-kaltura-player.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone https://github.com/kaltura/kaltura-player-js.git
-git checkout master
-yarn install
-repoName=$(npx -c 'echo "$npm_package_name"')
-git commit --allow-empty -m "Trigger Build Canary from $repoName"
-git push https://$1@github.com/kaltura/kaltura-player-js "master" > /dev/null 2>&1
