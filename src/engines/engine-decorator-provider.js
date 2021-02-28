4a52ead6a1d3a8d629a7f87f9ee9e662faf4aea5
yuval.keidar <keidaryu@gmail.com>
2021-02-28 14:41:17 +0200

    fix flow

--- src/engines/engine-decorator-provider.js
+++ src/engines/engine-decorator-provider.js
@@ -7,13 +7,16 @@
  * @implements {IEngineDecorator}
  */
 class EngineDecoratorProvider implements IEngineDecoratorProvider {
+  _name: string;
+  _getEngineDecorator: (engine: IEngine, dispatchEventHandler: Function) => IEngineDecorator;
+
   constructor(plugin: IEngineDecoratorProvider) {
     this._name = plugin.getName();
     this._getEngineDecorator = plugin.getEngineDecorator.bind(plugin);
   }
 
-  getEngineDecorator(...args): IEngineDecorator {
-    return this._getEngineDecorator(...args);
+  getEngineDecorator(engine: IEngine, dispatchEventHandler: Function): IEngineDecorator {
+    return this._getEngineDecorator(engine, dispatchEventHandler);
   }
 
   getName(): string {
