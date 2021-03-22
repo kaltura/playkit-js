5a6241898b8235eb0ca949aa5885d8f158bf0388
yuval.keidar <keidaryu@gmail.com>
2021-03-22 13:42:18 +0200

    fix condition

--- src/utils/restrictions.js
+++ src/utils/restrictions.js
@@ -11,15 +11,17 @@ import VideoTrack from '../track/video-track';
  * @returns {Array<VideoTrack>} - The relevant video tracks after restrictions.
  */
 function _filterVideoTracksByRestriction(tracks: Array<VideoTrack>, restriction: PKABRRestrictionObject): Array<VideoTrack> {
+  const MIN_DEFAULT_VALUE = 0;
+  const MAX_DEFAULT_VALUE = Infinity;
   const inRange = (x, min, max) => {
-    return x >= (min || 0) && x <= (max || Infinity);
+    return x >= (min || MIN_DEFAULT_VALUE) && x <= (max || MAX_DEFAULT_VALUE);
   };
   const {maxHeight, minHeight, maxWidth, minWidth} = restriction;
-  if (maxHeight || minHeight || maxWidth || minWidth) {
+  if (minHeight !== MIN_DEFAULT_VALUE || minWidth !== MIN_DEFAULT_VALUE || maxHeight !== MAX_DEFAULT_VALUE || maxWidth !== MAX_DEFAULT_VALUE) {
     return tracks.filter(track => inRange(track.height, minHeight, maxHeight)).filter(track => inRange(track.width, minWidth, maxWidth));
   }
   const {maxBitrate, minBitrate} = restriction;
-  if (minBitrate || maxBitrate) {
+  if (minBitrate !== MIN_DEFAULT_VALUE || maxBitrate !== MAX_DEFAULT_VALUE) {
     return tracks.filter(track => inRange(track.bandwidth, minBitrate, maxBitrate));
   }
   return tracks;
