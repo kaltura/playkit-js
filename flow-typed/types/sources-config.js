af70cab7a38eb64a09ac2fa79092808d0790ef16
yuval.keidar <keidaryu@gmail.com>
2021-05-05 12:13:36 +0300

    Update sources-config.js

--- flow-typed/types/sources-config.js
+++ flow-typed/types/sources-config.js
@@ -3,6 +3,7 @@ declare type PKSourcesConfigObject = {
   hls: Array<PKMediaSourceObject>,
   dash: Array<PKMediaSourceObject>,
   progressive: Array<PKMediaSourceObject>,
+  captions: Array<PKExternalCaptionObject>,
   options: PKMediaSourceOptionsObject,
   type: string,
   dvr: boolean,
