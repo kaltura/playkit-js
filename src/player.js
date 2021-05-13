90dc21747a361a76d1c128b549b654399c656c92
Yuval Keidar <48407737+Yuvalke@users.noreply.github.com>
2021-05-13 16:02:47 +0300

    Update src/player.js
    Co-authored-by: Yair Ansbacher <yair.ansbacher@kaltura.com>

--- src/player.js
+++ src/player.js
@@ -1298,7 +1298,7 @@ export default class Player extends FakeEventTarget {
    * @private
    */
   _applyABRRestriction(config: Object): void {
-    if (Utils.Object.hasPropertyPath(config, 'abr.restrictions') && this._engine && this._tracks) {
+    if (Utils.Object.hasPropertyPath(config, 'abr.restrictions') && this._engine && this._tracks.length) {
       const {restrictions} = this._config.abr;
       const videoTracks = this._tracks.filter(track => track instanceof VideoTrack);
       const newVideoTracks = filterTracksByRestriction(videoTracks, restrictions);
