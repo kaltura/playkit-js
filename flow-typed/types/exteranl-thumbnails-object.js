c5caeda0ce92c23d9ec545249e0a3f67afb60558
jonathangold <jonathan.gold@kaltura.com>
2021-08-15 09:52:37 +0300

    remove comments

--- flow-typed/types/exteranl-thumbnails-object.js
+++ flow-typed/types/exteranl-thumbnails-object.js
@@ -3,7 +3,3 @@ declare type PKExternalThumbnailsConfig = {
   imgBaseUrl?: string,
   vttUrl: string
 };
-
-// 1 the vtt file should contains th url to the img itself, if so, the imgUrl field is redundant here
-// 2 the config.sources.thumbnails should be an array like caption ?
-// 3 should be the same VTTCue as textTrack (with style) or different
