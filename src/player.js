34c7159c5c4b59a6ba71c380dfd1fa6373779b2b
yairansbacher <yairans@gmail.com>
2022-02-22 15:09:22 +0200

    const

--- src/player.js
+++ src/player.js
@@ -2548,10 +2548,10 @@ export default class Player extends FakeEventTarget {
     const activeTracks = this.getActiveTracks();
     const playbackConfig = this.config.playback;
     const offTextTrack: ?Track = this._getTextTracks().find(track => TextTrack.langComparer(OFF, track.language));
-    let currentOrConfiguredTextLang =
+    const currentOrConfiguredTextLang =
       this._playbackAttributesState.textLanguage ||
       this._getLanguage<TextTrack>(this._getTextTracks(), playbackConfig.textLanguage, activeTracks.text);
-    let currentOrConfiguredAudioLang =
+    const currentOrConfiguredAudioLang =
       this._playbackAttributesState.audioLanguage ||
       this._getLanguage<AudioTrack>(this._getAudioTracks(), playbackConfig.audioLanguage, activeTracks.audio);
     this._setDefaultTrack<TextTrack>(this._getTextTracks(), currentOrConfiguredTextLang, offTextTrack);
