ea28e37b62f879c38ce433e8451c6400590fa7cb
lianbenjamin <79077248+lianbenjamin@users.noreply.github.com>
2024-01-23 12:04:32 +0200

    fix(FEC-13594): playlist with youtube blocks interaction with player (#750)
    ### Description of the Changes
    
    - bug fix.
    
    **the issue:**
    when loading a playlist with youtube entry and media that has dualscreen, and playing the dualscreen media- the dualscreen player is not clickable. in addition, the interaction with the player gui area is also "blocked".
    
    **root cause:**
    we are adding `z-index = 1` to the engine element in case it is a youtube engine. when switching to the dual media (or just not a YT entry), it is blocking the interaction with the player in the gui area (play/pause), which includes blocking interaction with the secondary player (aka the dualscreen).
    
    **solution:**
    remove the value of `z-index` in case the engine is not youtube.
    
    Solves FEC-13594

--- src/player.ts
+++ src/player.ts
@@ -1792,6 +1792,9 @@ export default class Player extends FakeEventTarget {
       Utils.Dom.prependTo(engineEl, this._el);
       if (this._engine.id === 'youtube') {
         this._el.style.zIndex = '1';
+      } else if (this._el.style.zIndex) {
+        // in case the engine is not yt, need to remove the z-index value if exists
+        this._el.style.zIndex = '';
       }
     }
   }
