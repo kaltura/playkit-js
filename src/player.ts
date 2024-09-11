90f5ee5e61c340d609c77274fd5deeeb16a7b7ef
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2024-09-11 14:45:55 +0300

    change

--- src/player.ts
+++ src/player.ts
@@ -1280,6 +1280,13 @@ export default class Player extends FakeEventTarget {
     });
   }
 
+  /**
+   * change quality in child entries
+   * @function changeChildQuality
+   * @param {?Track} track - the track to change
+   * @returns {void}
+   * @public
+   */
   public changeChildQuality(track: any | string): void{
     if (track === 'auto') {
       this.enableAdaptiveBitrate();
