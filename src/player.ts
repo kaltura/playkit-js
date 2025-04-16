3327ab4373f396f8022cabe0c196214c755731dd
Roee Dean <roeedean@IL-ROEEDE-RND.local>
2025-04-16 18:52:18 +0300

    Fix for the event listen/unlisten

--- src/player.ts
+++ src/player.ts
@@ -2389,18 +2389,14 @@ export default class Player extends FakeEventTarget {
    */
   private _checkEndTime(): void {
     const endTime = this._sources.endTime;
-    let hasPausedAtEndTime = false;
     const onTimeUpdate = (): void => {
-      if (Math.floor(this.currentTime!) === endTime! && !hasPausedAtEndTime) {
+      if (Math.floor(this.currentTime!) === endTime!) {
         this.pause();
         this._eventManager.unlisten(this._engine, Html5EventType.TIME_UPDATE, onTimeUpdate);
-        hasPausedAtEndTime = true;
       }
     };
     if (endTime && typeof this.currentTime === 'number' && this.currentTime < endTime) {
-      this._eventManager.listen(this._engine, Html5EventType.TIME_UPDATE, () => {
-        onTimeUpdate();
-      });
+      this._eventManager.listen(this._engine, Html5EventType.TIME_UPDATE, onTimeUpdate);
     }
   }
 
