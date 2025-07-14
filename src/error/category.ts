534fc96da4b092cbf49bac0290b4eb29f911f15c
Tzipi-kaltura <tzipi.maksev@kaltura.com>
2025-07-14 15:37:27 +0300

    fix(SUP-48817): Entries Under Moderation Are Still Playing

--- src/error/category.ts
+++ src/error/category.ts
@@ -53,8 +53,10 @@ const Category: CategoryType = {
   SCHEDULED_RESTRICTED: 17,
 
   /** Access Control error */
+  ACCESS_CONTROL_BLOCKED: 18,
 
-  ACCESS_CONTROL_BLOCKED: 18
+  /** Moderation restriction error */
+  MODERATION_RESTRICTED: 19,
 };
 
 export {Category};
